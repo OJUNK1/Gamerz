bfeca6941f0e2b529932bf8b13ee10812081cc18
OJUNK1 <kozwns@gmail.com>
2023-08-24 01:29:05 +0900

    dd

--- powergamerz/src/main/java/co/yedam/gamerz/game/web/GameSearchController.java
+++ powergamerz/src/main/java/co/yedam/gamerz/game/web/GameSearchController.java
@@ -2,7 +2,6 @@ package co.yedam.gamerz.game.web;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -15,41 +14,33 @@ import co.yedam.gamerz.common.ViewResolve;
 import co.yedam.gamerz.game.service.GameService;
 import co.yedam.gamerz.game.service.GameVO;
 import co.yedam.gamerz.game.serviceImpl.GameServiceImpl;
-import co.yedam.gamerz.review.service.ReviewService;
-import co.yedam.gamerz.review.serviceImpl.ReviewServiceImpl;
 
-@WebServlet("/gameinfo.do")
-public class GameInfoController extends HttpServlet {
+@WebServlet("/gamesearch.do")
+public class GameSearchController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public GameInfoController() {
+	public GameSearchController() {
 		super();
+
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		GameService dao = new GameServiceImpl();
-		GameVO vo = new GameVO();
-		
-		vo.setGameId(Integer.valueOf(request.getParameter("gameId")));
-		
-		vo= dao.gameSelect(vo);
-		request.setAttribute("g", vo);
-		
-		
-		ReviewService dao2 = new ReviewServiceImpl();
-		List<HashMap<String, Object>> reviewList = new ArrayList<HashMap<String, Object>>();
-		
-		reviewList = dao2.reviewSelectList();
-		request.setAttribute("reviewlist", reviewList);
-		
-		String viewName = "game/gameinfo";
+		List<GameVO> games = new ArrayList<GameVO>();
+
+		String key = request.getParameter("gameName");
+
+		games = dao.gameSearchList(key);
+
+		request.setAttribute("games", games);
+
+		String viewName = "game/gamesearchresult";
 		ViewResolve.forward(request, response, viewName);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
 		doGet(request, response);
 	}
 
